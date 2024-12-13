230fc33254a4c2f2fd228378648fd394dbf33dd7
roisiano <63460832+roisiano@users.noreply.github.com>
2024-12-13 20:35:10 +0100

    Update EPG_script.sh

--- .github/workflows/EPG_script.sh
+++ .github/workflows/EPG_script.sh
@@ -5,6 +5,7 @@ sed -i '/^ *$/d' canales.txt
 
 rm -f EPG_temp*
 
+# Descargar y preparar EPG
 while IFS=, read -r epg
 do
     extension="${epg##*.}"
@@ -19,25 +20,31 @@ do
     cat EPG_temp00.xml >> EPG_temp.xml
 done < epgs.txt
 
+# Procesar canales
 while IFS=, read -r old new logo
 do
-    # Elimina las comillas externas de 'old' y 'new' si existen
+    # Eliminar comillas de old y new si existen
     old=$(echo "$old" | sed 's/^"//;s/"$//')
     new=$(echo "$new" | sed 's/^"//;s/"$//')
 
-    contar_channel="$(grep -c "channel=\"$old\"" EPG_temp.xml)"
-    if [ $contar_channel -gt 1 ]; then
+    echo "Buscando canal: $old"
+    contar_channel=$(grep -c "channel=\"$old\"" EPG_temp.xml)
+
+    if [ $contar_channel -gt 0 ]; then
+        echo "Canal encontrado: $old · Cambiando a: $new · Coincidencias: $contar_channel"
+
+        # Extraer sección del canal y modificarla
         sed -n "/<channel id=\"${old}\">/,/<\/channel>/p" EPG_temp.xml > EPG_temp01.xml
         sed -i '/<icon src/!d' EPG_temp01.xml
         if [ "$logo" ]; then
-            echo Nombre EPG: $old · Nuevo nombre: $new · Cambiando logo ··· $contar_channel coincidencias
+            echo "Cambiando logo a: $logo"
             echo '  </channel>' >> EPG_temp01.xml
             sed -i "1i\  <channel id=\"${new}\">" EPG_temp01.xml
             sed -i "2i\    <display-name>${new}</display-name>" EPG_temp01.xml
             sed -i "s#<icon src=.*#<icon src=\"${logo}\" \/>#" EPG_temp01.xml
             sed -i "3i\    <icon src=\"${logo}\" \/>" EPG_temp01.xml  				
         else
-            echo Nombre EPG: $old · Nuevo nombre: $new · Manteniendo logo ··· $contar_channel coincidencias
+            echo "Manteniendo logo existente."
             echo '  </channel>' >> EPG_temp01.xml
             sed -i "1i\  <channel id=\"${new}\">" EPG_temp01.xml
             sed -i "2i\    <display-name>${new}</display-name>" EPG_temp01.xml
@@ -45,6 +52,7 @@ do
         cat EPG_temp01.xml >> EPG_temp1.xml
         sed -i '$!N;/^\(.*\)\n\1$/!P;D' EPG_temp1.xml
 
+        # Modificar los programas asociados
         sed -n "/<programme.*\"${old}\"/,/<\/programme>/p" EPG_temp.xml > EPG_temp02.xml
         sed -i '/<programme/s/\">.*/\"/g' EPG_temp02.xml
         sed -i "s# channel=\"${old}\"##g" EPG_temp02.xml	
@@ -52,10 +60,11 @@ do
         sed -i ':a;N;$!ba;s/\nEPG_temp//g' EPG_temp02.xml
         cat EPG_temp02.xml >> EPG_temp2.xml
     else
-        echo Saltando canal: $old ··· $contar_channel coincidencias
+        echo "Saltando canal: $old · No encontrado en EPG."
     fi
 done < canales.txt
 
+# Generar nuevo archivo EPG
 date_stamp=$(date +"%d/%m/%Y %R")
 echo '<?xml version="1.0" encoding="UTF-8"?>' > miEPG.xml
 echo "<tv generator-info-name=\"miEPG $date_stamp\" generator-info-url=\"https://github.com/davidmuma/miEPG\">" >> miEPG.xml
@@ -64,5 +73,3 @@ cat EPG_temp2.xml >> miEPG.xml
 echo '</tv>' >> miEPG.xml
 
 rm -f EPG_temp*
-
-
